a1c0b9d26d2e6a4499f415c201407814738994af
Manikanta <kmanikanta@micron.com>
2019-10-01 12:06:51 +0530

    misc

--- string-reverse-service/src/config/express.js
+++ string-reverse-service/src/config/express.js
@@ -5,7 +5,7 @@ async function get(app, envConfig) {
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
 
-    app.listen(process.env.PORT || 8090);
+    app.listen(process.env.PORT || 8090, '0.0.0.0');
     console.log(`Server started , Running on ${process.env.PORT || 8090}`);
 
 };
