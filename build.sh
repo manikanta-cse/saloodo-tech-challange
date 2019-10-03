b531a5f444de828fb9e08d003ac535a5bda79d38
root <root@k8s-node.exqfjqcfs3nunhgdnon1zgtsra.ix.internal.cloudapp.net>
2019-10-03 05:19:09 +0000

    added build script

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+
+docker_version=$(docker -v)
+echo "$docker_version"
+
+string_reverse_image="string-reverse"
+random_num_rev_str_image="random-num-reverse-str"
+
+if [[ $docker_version == *"build"* ]]; then
+   echo "docker found on machine"
+   echo "starting to build images..."
+   docker build -t $string_reverse_image ./string-reverse-service
+   docker build -t $random_num_rev_str_image ./random-num-reverse-str-service
+   echo "docker build is done"
+else
+    echo  "docker not found on machine, so exiting!"
+    exit 0
+fi
